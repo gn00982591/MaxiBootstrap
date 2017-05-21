8e9f6db222d0d2bcb469b1ae915aad3117f7d845
maximuschang <gn00982591@msn.com>
2017-05-21 17:32:29 +0800

    2017052102

--- Scripts/jquery.ext.js
+++ Scripts/jquery.ext.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(window).on("load", function () {
+    /*ajax 全域設定*/
+    $.ajaxSetup({ "global": true });
+    $(document)
+        .ajaxComplete(function (event, xhr, settings) { /*setTimeout(function () { $.unblockUI(); }, 100);*/ })
+        .ajaxSuccess(function () { /*$("#debug").text("");*/ })
+        .ajaxError(function (event, jqxhr, settings, thrownError) {
+            $("#dvbug").text(jqxhr.responseText);
+            location.href = "#dverror";
+        });
+});
