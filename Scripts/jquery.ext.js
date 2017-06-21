38842b8dce93da32b60d7f99c4fca0fd65474b9d
gn00982591 <gn00982591@msn.com>
2017-06-21 14:59:15 +0800

    2017062101

--- Scripts/jquery.ext.js
+++ Scripts/jquery.ext.js
@@ -1,4 +1,25 @@
-﻿$(window).on("load", function () {
+﻿(function ($, undefined) {
+    /*$*/
+    /*by element*/
+    $.fn.extend({
+        /*form元件轉object*/
+        "serializeobject": function () {
+            var o = {};
+            $(this).find('input[type="hidden"], input[type="date"], input[type="number"], input[type="text"], input[type="password"], input[type="checkbox"], input[type="radio"], select ,textarea').each(function () {
+                var $th = $(this)
+                if (this.name === null || this.name === undefined || this.name === '') { return; }
+                var elemValue = '';
+                if ($th.is('select')) { elemValue = $th.find('option:selected').val(); }
+                else if ($th.is('input[type="checkbox"]') || $th.is('input[type="radio"]')) { elemValue = $th.is(":checked"); }
+                else { elemValue = this.value; }
+                $(o).prop(this.name, elemValue);
+            });
+            return o;
+        },
+    });
+})(jQuery);
+
+$(window).on("load", function () {
     /*ajax 全域設定*/
     $.ajaxSetup({ "global": true });
     $(document)
